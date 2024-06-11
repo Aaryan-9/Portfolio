4b60d22d47f9564ebc7d8af8de5fd2bc30862212
Aaryan-9 <2022kucp1031@iiitkota.ac.in>
2024-06-11 13:20:28 +0530

    13th

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -24,69 +24,70 @@ function App() {
 
     return () => clearTimeout(timer);
   }, []);
+
+  if (loading) {
+    return (
+      <div className="h-screen w-screen bg-black flex justify-center items-center">
+        <div className="w-1/2 md:w-[20%]">
+          <Lottie animationData={Loader} />
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      {loading ? (
-        <div className="flex bg-black justify-center items-center h-screen w-screen">
-          <div className="w-1/2 md:w-[20%]">
-            <Lottie animationData={Loader} />
+    <>
+      <div class="flex flex-col justify-center">
+        <Navbar />
+        <div className="md:grid xl:grid-rows-7 mb-5 xl:grid-cols-7 xl:px-40  lg:h-[75rem] xl:h-full gap-5 lg:px-30  lg:grid-cols-5 lg:grid-rows-8 md:px-10 md:grid-rows-3 md:grid-cols-3 ">
+          <div className="xl:col-span-2 xl:row-span-5  max-xl:hidden sm:visible contain-content  ">
+            <div className="xl:mt-[3.2rem] ">
+              <Photo />
+            </div>
+          </div>
+          <div className="xl:col-span-5 xl:row-span-3   contain-content lg:col-span-5 lg:row-span-3 md:col-span-3">
+            <div className="xl:mt-[3.2rem] ">
+              <About />
+            </div>
+          </div>
+          <div className="xl:row-span-2 xl:col-span-1 lg:col-span-2 lg:row-span-4 md:col-span-1 ">
+            <Resume />
+          </div>
+          <div className="xl:col-span-4 xl:row-span-4 contain-content lg:col-span-3 lg:row-span-4 md:col-span-2">
+            <TechStack />
+          </div>
+          <div className="xl:col-span-3 xl:row-span-2 lg:col-span-5 md:col-span-3">
+            <Skills />
           </div>
         </div>
-      ) : (
-        <>
-          <div class="flex flex-col justify-center">
-            <Navbar />
-            <div className="md:grid xl:grid-rows-7 mb-5 xl:grid-cols-7 xl:px-40  lg:h-[75rem] xl:h-full gap-5 lg:px-30  lg:grid-cols-5 lg:grid-rows-8 md:px-10 md:grid-rows-3 md:grid-cols-3 ">
-              <div className="xl:col-span-2 xl:row-span-5  max-xl:hidden sm:visible contain-content  ">
-                <div className="xl:mt-[3.2rem] ">
-                  <Photo />
-                </div>
-              </div>
-              <div className="xl:col-span-5 xl:row-span-3   contain-content lg:col-span-5 lg:row-span-3 md:col-span-3">
-                <div className="xl:mt-[3.2rem] ">
-                  <About />
-                </div>
-              </div>
-              <div className="xl:row-span-2 xl:col-span-1 lg:col-span-2 lg:row-span-4 md:col-span-1 ">
-                <Resume />
-              </div>
-              <div className="xl:col-span-4 xl:row-span-4 contain-content lg:col-span-3 lg:row-span-4 md:col-span-2">
-                <TechStack />
-              </div>
-              <div className="xl:col-span-3 xl:row-span-2 lg:col-span-5 md:col-span-3">
-                <Skills />
-              </div>
-            </div>
-            <div
-              id="projects"
-              className="xl:px-28 md:px-10 lg:px-20 lg:mt-40 xl:mt-20"
-            >
-              <Projects />
-            </div>
-            <div className="md:mt-12 md:grid md:grid-rows-3 md:grid-cols-3 xl:px-40 lg:px-30 md:px-10 h-[35rem] gap-5  ">
-              <div className="md:col-span-1 contain-content ">
-                <Links />
-              </div>
-              <div className="col-span-2 row-span-3 contain-content">
-                <Spotify />
-              </div>
-              <div className="md:col-span-1 md:row-span-2 contain-content">
-                <Quotes />
-              </div>
-            </div>
-            <div
-              id="contact"
-              className="max-sm:mt-[36rem] max-sm:ml-4 max-sm:mr-4 mt-24"
-            >
-              <Contact />
-            </div>
-            <footer className="h-[5rem] mt-32">
-              <Footer />
-            </footer>
+        <div
+          id="projects"
+          className="xl:px-28 md:px-10 lg:px-20 lg:mt-40 xl:mt-20"
+        >
+          <Projects />
+        </div>
+        <div className="md:mt-12 md:grid md:grid-rows-3 md:grid-cols-3 xl:px-40 lg:px-30 md:px-10 h-[35rem] gap-5  ">
+          <div className="md:col-span-1 contain-content ">
+            <Links />
           </div>
-        </>
-      )}
-    </div>
+          <div className="col-span-2 row-span-3 contain-content">
+            <Spotify />
+          </div>
+          <div className="md:col-span-1 md:row-span-2 contain-content">
+            <Quotes />
+          </div>
+        </div>
+        <div
+          id="contact"
+          className="max-sm:mt-[36rem] max-sm:ml-4 max-sm:mr-4 mt-24"
+        >
+          <Contact />
+        </div>
+        <footer className="h-[5rem] mt-32">
+          <Footer />
+        </footer>
+      </div>
+    </>
   );
 }
 
